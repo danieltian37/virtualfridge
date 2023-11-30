b101900823962436dfc7222ac38f2e0062c45991
danieltian37 <danieltian0307@gmail.com>
2023-11-30 18:50:04 -0500

    yay final final

--- part1/part1/src/components/FridgeItem.jsx
+++ part1/part1/src/components/FridgeItem.jsx
@@ -1,26 +1,50 @@
 import apple from '../assets/apple.svg'
 import milk from '../assets/milk.svg'
 import eggs from '../assets/eggs.svg'
+import React, { createContext, useContext, useEffect, useState } from 'react';
+
+const ExpPopup = () => {
+    console.log("loggers")
+    return (
+        <>
+            <h1>
+                <div className="popup">
+                    <p>your item is about to expire!</p>
+                </div>
+            </h1>
+        </>
+    )
+}
 
 const FridgeItem = ({ name }) => {
-    /**
-     * create function that returns image that you import with import
-     * 
-     * given name, return the name's image
-     */
+    const [isHovering, setIsHovering] = useState(false);
+    const handleMouseOver = () => {
+        setIsHovering(true);
+    }
+    const handleMouseOut = () => {
+        setIsHovering(false);
+    }
 
-    /**
-     * switch statement to return the correct image
-     */
+    const [state, setState] = useState({x: 0, y: 0});
+    const handleMouseMove = (e) => {
+       setState({ x: e.screenX, y: e.screenY });
+    }
+    
     switch (name) {
         case 'apple':
-            return <img src={apple} className= { name } alt = { name }/>
+            return <img src={apple} className= { name } class="apple" alt = { name }/>
         case 'milk':
-            return <img src={milk} className= { name } alt = { name }/>
+            return <>
+                    <img src={milk} className= { name } class="milk" alt = { name }
+                    onMouseEnter={handleMouseOver}
+                    onMouseLeave={handleMouseOut}
+                    onMouseMove={handleMouseMove}/>
+                    {isHovering && <ExpPopup/>}
+                </>
         case 'eggs':
-            return <img src={eggs} className= { name } alt = { name }/>
+            return <img src={eggs} className= { name } class="eggs" alt = { name }/>
         default:
-            return <img src={name} className= { name } alt = { name }/>
+            return <img src={name} className= { name } class={ name } alt = { name }/>
     }
 }
 
